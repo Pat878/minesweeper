957f2d37a596acb518560b7486ffc3641d651a37
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-06-29 07:04:53 -0400

    Add data ids to cells

--- script.js
+++ script.js
@@ -10,5 +10,8 @@ var makeGrid  = (function () {
         $(".divTableBody").append("<div class='divTableRow'></div>") }
       for (i=0;i<row;i++){
         $(".divTableRow").append("<div class='divTableCell'></div>") }
+        $(".divTableCell").each( function(i) {
+              $(this).attr('data', (i+1));
+            });
     };
 })();
